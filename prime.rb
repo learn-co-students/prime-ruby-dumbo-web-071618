513dd1d09bf554c90e5a4f5764e9328a17181a66
Vadim Avnilov <vadim.avnilov@gmail.com>
2018-07-28 19:32:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif  number ==2 || number == 3
+    true
+  else
+    array = (2...number).to_a
+    array.each do |element|
+      return false if number % element == 0
+    end
+    true
+  end
+end
